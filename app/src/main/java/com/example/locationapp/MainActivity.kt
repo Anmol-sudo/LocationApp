ef0853a029be3f5c6445b38b901a30424a218a0f
Anmol-Sudo <anmolshiv1974@gmail.com>
2025-06-24 10:57:20 +0530

    feat: Implement location updates
    This commit introduces location update functionality.
    
    The `LocationUtils` class now includes:
    - A `FusedLocationProviderClient` to interact with the fused location provider.
    - A `requestLocationUpdates` function that:
        - Creates a `LocationCallback` to receive location updates.
        - Updates the `LocationViewModel` with the new `LocationData` when a location result is available.
        - Creates a high-accuracy `LocationRequest`.
        - Requests location updates using the `_fusedLocationClient`.
    
    The `LocationDisplay` composable has been updated to:
    - Request location updates via `locationUtils.requestLocationUpdates(viewModel)`:
        - After permissions are granted in the `requestPermissionLauncher` callback.
        - When the "Get Location" button is clicked and permissions are already granted.
    - Display the latitude and longitude from the `viewModel.location.value` if available, otherwise it shows "Location not available".

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -55,12 +55,15 @@ fun LocationDisplay(
     viewModel: LocationViewModel,
     context: Context
 ) {
+    val location = viewModel.location.value
 
     val requestPermissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(),
         onResult = { permissions ->
             if(permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true && permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true){
                 // I HAVE ACCESS FOR LOCATION
+
+                locationUtils.requestLocationUpdates(viewModel = viewModel)
             }else{
                 // Ask for permission
                 val rationaleRequired = ActivityCompat.shouldShowRequestPermissionRationale(
@@ -89,11 +92,17 @@ fun LocationDisplay(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
         ) {
-        Text(text = "Location not available")
+
+        if(location != null){
+            Text("Address: ${location.latitude} ${location.longitude}")
+        }else{
+            Text(text = "Location not available")
+        }
 
         Button(onClick = {
             if(locationUtils.hasLocationPermission(context)){
                 // Permission already granted update the location
+                locationUtils.requestLocationUpdates(viewModel)
             }else{
                 // Request location permission
                 requestPermissionLauncher.launch(
